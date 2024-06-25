7cc0bbfc2c8dbf6deab661791aa1138fe88a3015
Gibbs <mr.darmstadtiumgmail.com>
2024-06-25 08:23:32 +0200

    add ticket, reservation, planetarium dome, show session models. Add functionality for validating tickets, add _str_ method to models

--- user/models.py
+++ user/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
