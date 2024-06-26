367bcecfcab4adf95ebf0a58d444331a91bcc4b9
Gibbs <mr.darmstadtiumgmail.com>
2024-06-26 18:22:01 +0200

    rewrite User for using email instead of username. Add endpointstake token, refresh it, verify it.

--- planetarium_service/settings.py
+++ planetarium_service/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "planetarium",
     "user",
-    "rest_framework"
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
--- user/views.py
+++ user/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 
 from user.serializers import UserSerializer
 
