15175d9a3fb53b0d6527434726e81a25f986c5aa
Gibbs <mr.darmstadtiumgmail.com>
2024-07-27 09:04:58 +0200

    secret key remowed from setting to .env, add instructions to .env.example

--- planetarium_service/settings.py
+++ planetarium_service/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-nbge_b0&!k(9n&trv4=y$+i@xy^j&!^kpeuh#h6x&66w!1e(**"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don"t run with debug turned on in production!
 
